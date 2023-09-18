b5c2b55350527d7d9663d570a10cbe5e807d5cd8
ZodiusInfuser <christopher.parrott2@gmail.com>
2023-09-18 18:46:10 +0100

    Added current limit adjustment to Dual Motor

--- lib/pimoroni_yukon/modules/dual_motor.py
+++ lib/pimoroni_yukon/modules/dual_motor.py
@@ -7,6 +7,18 @@ from pwmio import PWMOut
 from digitalio import DigitalInOut
 from collections import OrderedDict
 from pimoroni_yukon.errors import FaultError, OverTemperatureError
+import pimoroni_yukon.logging as logging
+
+# The current (in amps) associated with each limit (Do Not Modify!)
+CURRENT_LIMIT_1 = 0.161
+CURRENT_LIMIT_2 = 0.251
+CURRENT_LIMIT_3 = 0.444
+CURRENT_LIMIT_4 = 0.786
+CURRENT_LIMIT_5 = 1.143
+CURRENT_LIMIT_6 = 1.611
+CURRENT_LIMIT_7 = 1.890
+CURRENT_LIMIT_8 = 2.153
+CURRENT_LIMIT_9 = 2.236
 
 
 class DualMotorModule(YukonModule):
@@ -17,6 +29,7 @@ class DualMotorModule(YukonModule):
     NUM_STEPPERS = 1
     FAULT_THRESHOLD = 0.1
     DEFAULT_FREQUENCY = 25000
+    DEFAULT_CURRENT_LIMIT = CURRENT_LIMIT_3
     TEMPERATURE_THRESHOLD = 50.0
 
     # | ADC1  | SLOW1 | SLOW2 | SLOW3 | Module               | Condition (if any)          |
@@ -26,13 +39,27 @@ class DualMotorModule(YukonModule):
     def is_module(adc_level, slow1, slow2, slow3):
         return adc_level == ADC_HIGH and slow1 is LOW and slow2 is LOW and slow3 is HIGH
 
-    def __init__(self, motor_type=DUAL, frequency=DEFAULT_FREQUENCY):
+    def __init__(self, motor_type=DUAL, frequency=DEFAULT_FREQUENCY, current_limit=DEFAULT_CURRENT_LIMIT):
         super().__init__()
         self.__motor_type = motor_type
         if self.__motor_type == self.STEPPER:
             self.NAME += " (Stepper)"
 
         self.__frequency = frequency
+        self.__current_limit = current_limit
+
+        # An ascending order list of current limits with the pin states to achieve them
+        self.__current_limit_states = OrderedDict({
+            CURRENT_LIMIT_1: (0, 0),
+            CURRENT_LIMIT_2: (-1, 0),
+            CURRENT_LIMIT_3: (0, -1),
+            CURRENT_LIMIT_4: (1, 0),
+            CURRENT_LIMIT_5: (-1, -1),
+            CURRENT_LIMIT_6: (0, 1),
+            CURRENT_LIMIT_7: (1, -1),
+            CURRENT_LIMIT_8: (-1, 1),
+            CURRENT_LIMIT_9: (1, 1),
+        })
 
     def initialise(self, slot, adc1_func, adc2_func):
         try:
@@ -58,9 +85,9 @@ class DualMotorModule(YukonModule):
             self.stepper = StepperMotor(self.__pwms_p[0], self.__pwms_n[0], self.__pwms_p[1], self.__pwms_n[1])
 
         # Create motor control pin objects
-        self.__motors_decay = DigitalInOut(slot.SLOW1)
-        self.__motors_toff = DigitalInOut(slot.SLOW2)
         self.__motors_en = DigitalInOut(slot.SLOW3)
+        self.__motors_vref1 = DigitalInOut(slot.SLOW1)
+        self.__motors_vref2 = DigitalInOut(slot.SLOW2)
 
         # Pass the slot and adc functions up to the parent now that module specific initialisation has finished
         super().initialise(slot, adc1_func, adc2_func)
@@ -72,9 +99,8 @@ class DualMotorModule(YukonModule):
         else:
             self.stepper.release()
 
-        self.__motors_decay.switch_to_output(False)
-        self.__motors_toff.switch_to_output(False)
         self.__motors_en.switch_to_output(False)
+        self.current_limit(self.__current_limit)
 
     def enable(self):
         self.__motors_en.value = True
@@ -85,17 +111,41 @@ class DualMotorModule(YukonModule):
     def is_enabled(self):
         return self.__motors_en.value
 
-    def decay(self, value=None):
-        if value is None:
-            return self.__motors_decay
+    def current_limit(self, amps):
+        if amps is None:
+            return self.__current_limit
         else:
-            self.__motors_decay = value
+            if self.is_enabled():
+                raise RuntimeError("Cannot change current limit whilst motor driver is active")
 
-    def toff(self, value=None):
-        if value is None:
-            return self.__motors_toff
-        else:
-            self.__motors_toff = value
+            # Start with the lowest limit, e
+            chosen_limit = CURRENT_LIMIT_1
+            chosen_state = self.__current_limit_states[CURRENT_LIMIT_1]
+
+            # Find the closest current limit below the given amps value
+            for limit, state in self.__current_limit_states.items():
+                if limit < amps:
+                    break
+                chosen_limit = limit
+                chosen_state = state
+
+            if chosen_state[0] is -1:
+                self.__motors_vref1.switch_to_input()
+            elif chosen_state[0] is 0:
+                self.__motors_vref1.switch_to_output(False)
+            else:
+                self.__motors_vref1.switch_to_output(True)
+
+            if chosen_state[1] is -1:
+                self.__motors_vref2.switch_to_input()
+            elif chosen_state[1] is 0:
+                self.__motors_vref2.switch_to_output(False)
+            else:
+                self.__motors_vref2.switch_to_output(True)
+
+            self.__current_limit = chosen_limit
+
+            logging.info(self.__message_header() + f"Current limit set to {self.__current_limit}A")
 
     @property
     def motor1(self):
